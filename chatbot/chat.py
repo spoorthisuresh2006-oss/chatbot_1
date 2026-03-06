e6e1efc352bee90cc2c7571b75f688c766a030a7
Spoorthi Suresh <spoorthisuresh2006@gmail.com>
2026-03-06 12:03:57 +0530

    hi

---
+++ chatbot/chat.py
@@ -0,0 +1,13 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="what is the color of the sky",
+)
+
+print(response.text)
